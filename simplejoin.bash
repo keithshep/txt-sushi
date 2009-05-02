2bfe1f32a9dc79594c94bd19df9b4e358c971e90
keithshep <keithshep@gmail.com>
2009-05-02 14:37:15 -0400

    mv example1.csv to example2.csv
    Ignore-this: d53468373d5a3fd903dc3ab1d8292dc2
    
    darcs-hash:20090502183715-b3b23-ed745f36e0f2e0272bdd8413b685bdb1152c1392.gz

--- simplejoin.bash
+++ simplejoin.bash
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-./tssql 'select `example.csv`.*, `example2.csv`.`other val`, `example2.csv`.id2 from `example.csv` join `example2.csv` on `example.csv`.id = `example2.csv`.id2'
+./tssql 'select `example1.csv`.*, `example2.csv`.`other val`, `example2.csv`.id2 from `example1.csv` join `example2.csv` on `example1.csv`.id = `example2.csv`.id2'
+
