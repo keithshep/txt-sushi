26d5bd54a4cda83bc5c3c5085e7c69888458b99b
keithshep <keithshep@gmail.com>
2009-05-05 22:50:33 -0400

    full path to tssql
    Ignore-this: 1a365063ce113e0d91b2a50f14e98c
    
    darcs-hash:20090506025033-b3b23-60fd886e87aa0ef334cf1591ff1e059ab7b79f39.gz

--- simplejoin.bash
+++ simplejoin.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./tssql 'select `example1.csv`.*, `example2.csv`.`other val`, `example2.csv`.id2 from `example1.csv` join `example2.csv` on `example1.csv`.id = `example2.csv`.id2'
+./dist/build/tssql/tssql 'select `example1.csv`.*, `example2.csv`.`other val`, `example2.csv`.id2 from `example1.csv` join `example2.csv` on `example1.csv`.id = `example2.csv`.id2'
 
