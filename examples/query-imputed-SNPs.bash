5f1f1bac9e01b177721cb12db753f39d4d568298
keithshep <keithshep@gmail.com>
2009-05-17 23:37:15 -0400

    added to examples
    Ignore-this: 44fa35b1899f1208f6ee73c58b4263f3
    
    darcs-hash:20090518033715-b3b23-501a478d26cb3669ed30e4d5a473cec3634efe6d.gz

---
+++ examples/query-imputed-SNPs.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# An example of using TxtSushi to query one of the CGD (center for genome dynamics)
+# datasets
+# 
+# Selects chromosome, base pair position, data source and a couple of strains.
+# Filters out any rows from perlegen and any rows where A/J and BPH/2J differ
+
+curl -s http://cgd.jax.org/ImputedSNPData/v1.1/PBupdatedv1.1_HMM_filleddata/PBupdatedv1.1_SNP_genoData_chr4.csv.gz \
+| zcat \
+| tssql -table cgd - \
+'select ChrID, `build 36 bp Position`, Source, `BPH/2J`, `A/J` from cgd
+where Source <> "Perlegen36_b03" and `A/J` = `BPH/2J`
+order by `build 36 bp Position` + 0 desc' \
+| csvtopretty -
--- examples/query-mgi-markers.bash
+++ examples/query-mgi-markers.bash
@@ -2,6 +2,12 @@
 
 # An example of using TxtSushi to query one of the MGI (mouse genome informatics)
 # database reports
+#
+# Selects accession ID, Symbol, chromosome, ID and centimorgan position (without
+# the "trim" we end up with extra spaces).
+# Only select chromosomes 1, 8 and 19 and look for "N/A" positions
+# Sort the rows by chromosome (the "+0" coerces the chromosome from text to an
+# integer) then symbol
 
 wget -q -O - ftp://ftp.informatics.jax.org/pub/reports/MRK_List2.rpt | tabtocsv - \
 | tssql -table mgi - \
