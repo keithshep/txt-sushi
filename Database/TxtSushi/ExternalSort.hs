d5ed94a74848bcd1505da9a78c8e59f96dcbe4cf
keithshep <keithshep@gmail.com>
2009-07-11 15:24:56 -0400

    added max open files constraint
    Ignore-this: fddb7ec00bdc8296f43eaf9a2176369f
    
    darcs-hash:20090711192456-b3b23-3b0cb86c6bc074f4501a1f35cd5aadbdca21b1ea.gz

--- Database/TxtSushi/ExternalSort.hs
+++ Database/TxtSushi/ExternalSort.hs
@@ -1,6 +1,9 @@
 module Database.TxtSushi.ExternalSort (
+    externalSort,
     externalSortBy,
-    externalSortByQuota) where
+    externalSortByConstrained,
+    defaultByteQuota,
+    defaultMaxOpenFiles) where
 
 import Database.TxtSushi.IO
 import Database.TxtSushi.Transform
@@ -15,68 +18,72 @@ import System.IO
 import System.IO.Unsafe
 import System.Directory
 
--- | performs an external sort on the given list
+
+-- | performs an external sort on the given list using the default resource
+--   constraints
 externalSort :: (Binary b, Ord b) => [b] -> [b]
 externalSort = externalSortBy compare
 
 -- | performs an external sort on the given list using the given comparison
---   function
+--   function and the default resource constraints
 externalSortBy :: (Binary b) => (b -> b -> Ordering) -> [b] -> [b]
-externalSortBy = externalSortByQuota defaultByteQuota
-    -- 128 MB is the default quota
-    where defaultByteQuota = 64 * 1024 * 1024
+externalSortBy = externalSortByConstrained defaultByteQuota defaultMaxOpenFiles
 
--- | performs an external sort on the given list using the given comparison
---   function
-externalSortByQuota :: (Binary b, Integral i) => i -> (b -> b -> Ordering) -> [b] -> [b]
-externalSortByQuota byteQuota cmp xs = unsafePerformIO $ do
+-- | Currently 8 MB. Don't rely on this value staying the same in future
+--   releases!
+defaultByteQuota :: (Integral i) => i
+defaultByteQuota = 8 * 1024 * 1024
+
+-- | Currently 16 files. Don't rely on this value staying the same in future
+--   releases!
+defaultMaxOpenFiles :: (Integral i) => i
+defaultMaxOpenFiles = 8
+
+-- | performs an external sort on the given list using the given resource
+--   constraints
+externalSortByConstrained :: (Binary b, Integral i) => i -> i -> (b -> b -> Ordering) -> [b] -> [b]
+externalSortByConstrained byteQuota maxOpenFiles cmp xs = unsafePerformIO $ do
     partialSortFiles <- bufferPartialSortsBy (fromIntegral byteQuota) cmp xs
-    partialSortFileHandles <-
-        unwrapMonadList [openBinaryFile file ReadMode | file <- partialSortFiles]
-    partialSortByteStrs <-
-        unwrapMonadList $ map BS.hGetContents partialSortFileHandles
+    partialSortByteStrs <- readBinFiles partialSortFiles
     
     let partialSortBinaries = map decodeAll partialSortByteStrs
-    return $ mergeAllBy cmp partialSortBinaries
+    
+    -- now we must merge together the partial sorts
+    --return $ mergeAllBy cmp partialSortBinaries
+    externalMergeAllBy (fromIntegral maxOpenFiles) cmp partialSortBinaries
 
 -- | unwrap a list of Monad 'boxed' items
-unwrapMonadList [] = return []
-unwrapMonadList (ioHead:ioTail) = do
-    unwrappedHead <- ioHead
-    unwrappedTail <- unwrapMonadList ioTail
+unwrapMonads [] = return []
+unwrapMonads (x:xt) = do
+    unwrappedHead <- x
+    unwrappedTail <- unwrapMonads xt
     return (unwrappedHead:unwrappedTail)
 
 -- | merge a list of sorted lists into a single sorted list
 mergeAllBy :: (a -> a -> Ordering) -> [[a]] -> [a]
 mergeAllBy _ [] = []
+mergeAllBy _ [singletonList] = singletonList
+mergeAllBy cmp (fstList:sndList:[]) = mergeBy cmp fstList sndList
 mergeAllBy cmp listList =
-    let mergedPairs = mergePairsBy cmp listList
-    in
-        case mergedPairs of
-            [singletonListHead] -> singletonListHead
-            _                   -> mergeAllBy cmp mergedPairs
+    -- recurse after breking the list down by about 1/2 the size
+    mergeAllBy cmp (partitionAndMerge 2 cmp listList)
 
--- TODO add a smart adjustment bin size so that the last bin will not be like 2
--- when the others are 16
+-- TODO add a smart adjustment so that the last partition will not ever
+--      be more than 1 element different than the others
 
--- | merge a list of sorted lists into a single sorted list
-mergePartitionsBy :: Int -> (a -> a -> Ordering) -> [[a]] -> [[a]]
-mergePartitionsBy _ _ [] = []
-mergePartitionsBy partitionSize cmp listList =
-    map (mergeAllBy cmp) $ map (partitionList partitionSize) listList
+-- | partitions the given sorted lists into groupings containing `partitionSize`
+--   or fewer lists then merges each of those partitions
+partitionAndMerge :: Int -> (a -> a -> Ordering) -> [[a]] -> [[a]]
+partitionAndMerge _ _ [] = []
+partitionAndMerge partitionSize cmp listList =
+    map (mergeAllBy cmp) $ regularPartitions partitionSize listList
 
-partitionList :: Int -> [a] -> [[a]]
-partitionList _ [] = []
-partitionList partitionSize xs =
+-- | chops up the given list at regular intervals
+regularPartitions :: Int -> [a] -> [[a]]
+regularPartitions _ [] = []
+regularPartitions partitionSize xs =
     let (currPartition, otherXs) = splitAt partitionSize xs
-    in  currPartition : partitionList partitionSize otherXs
-
--- | merge the sorted lists in the list to a list about 1/2 the size
-mergePairsBy _ [] = []
-mergePairsBy cmp singletonListList@(headList:[]) = singletonListList
-mergePairsBy cmp (list1:list2:listListTail) =
-    let mergedPair = mergeBy cmp list1 list2
-    in  mergedPair:(mergePairsBy cmp listListTail)
+    in  currPartition : regularPartitions partitionSize otherXs
 
 -- | merge two sorted lists into a single sorted list
 mergeBy :: (a -> a -> Ordering) -> [a] -> [a] -> [a]
@@ -86,6 +93,24 @@ mergeBy comparisonFunction list1@(head1:tail1) list2@(head2:tail2) =
     case head1 `comparisonFunction` head2 of
         GT  -> (head2:(mergeBy comparisonFunction list1 tail2))
         _   -> (head1:(mergeBy comparisonFunction tail1 list2))
+
+externalMergeAllBy :: Binary b => Int -> (b -> b -> Ordering) -> [[b]] -> IO [b]
+externalMergeAllBy _ _ [] = return []
+-- TODO do i need to write singleton lists to file in order to keep the max open file promise??
+externalMergeAllBy _ _ [singletonList] = return singletonList
+externalMergeAllBy maxOpenFiles cmp listList = do
+    -- we use (maxOpenFiles - 1) because we need to account for the file
+    -- handle that we're reading from
+    let mergedPartitions = partitionAndMerge (maxOpenFiles - 1) cmp listList
+    
+    -- Stream the results through the file system (we don't want to hold
+    -- the results in memory)
+    mergedBuffFiles <- unwrapMonads $ map bufferToTempFile mergedPartitions
+    mergedByteStrs <- readBinFiles mergedBuffFiles
+    let mergedBinaries = map decodeAll mergedByteStrs
+    
+    -- time to recurse
+    externalMergeAllBy maxOpenFiles cmp mergedBinaries
 
 -- | create a list of parial sorts
 bufferPartialSortsBy :: (Binary b) => Int64 -> (b -> b -> Ordering) -> [b] -> IO [String]
@@ -97,6 +122,10 @@ bufferPartialSortsBy byteQuota cmp xs = do
     otherSortBuffers <- bufferPartialSortsBy byteQuota cmp sortLaterList
     return (sortBuffer:otherSortBuffers)
 
+-- TODO not efficiet! we're converting to binary twice so that we don't have
+-- the bytestrings buffered to memory during the sort (that would about double
+-- our mem usage). I think the right answer is to add a class extending binary
+-- that has a sizeOf function
 splitAfterQuota :: (Binary b) => Int64 -> [b] -> ([b], [b])
 splitAfterQuota _ [] = ([], [])
 splitAfterQuota quotaInBytes (binaryHead:binaryTail) =
@@ -108,26 +137,12 @@ splitAfterQuota quotaInBytes (binaryHead:binaryTail) =
         then ([binaryHead], binaryTail)
         else (binaryHead:fstBinsTail, sndBins)
 
-{-
--- | Encode the given binaries respecting the given byte quota (Well, kind
---   of respecting. Really we'll probably go over by one element)
-encodeByQuota :: (Binary b, Integral i) => i -> [b] -> (BS.ByteString, [b])
-encodeByQuota byteQuota binaries =
-    let (byteStrings, binsLeft) = go (fromIntegral byteQuota) binaries
-    in  (BS.concat byteStrings, binsLeft)
-    where
-        -- the go function does all of the real work
-        go _ [] = ([], [])
-        go quota (binHead:binTail) =
-            let
-                headByteStr = encode binHead
-                quotaRemaining = quota - BS.length headByteStr
-                (tailByteStrs, binsLeft) = go quotaRemaining binTail
-            in
-                if quotaRemaining >= 0
-                then (headByteStr:tailByteStrs, binsLeft)
-                else ([headByteStr], binTail)
--}
+-- | lazily reads the given binary files
+readBinFiles :: [String] -> IO [BS.ByteString]
+readBinFiles fileNames = do
+    fileHandles <- unwrapMonads [openBinaryFile file ReadMode | file <- fileNames]
+    byteStrs <- unwrapMonads $ map BS.hGetContents fileHandles
+    return byteStrs
 
 -- | buffer the binaries to a temporary file and return a handle to that file
 bufferToTempFile :: (Binary b) => [b] -> IO String
