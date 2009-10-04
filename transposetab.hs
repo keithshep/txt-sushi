3b187995c8aafc4f322f10a17d48bf2cc1833181
keithshep <keithshep@gmail.com>
2009-10-04 16:56:56 -0400

    added transposetab utility
    Ignore-this: 396708f061309f946f2486dc5cef89c7
    
    darcs-hash:20091004205656-b3b23-88b4522064c19927c06e8b0d4665f8de9d64da07.gz

---
+++ transposetab.hs
@@ -0,0 +1,21 @@
+import Data.List
+import System.Environment
+import System.IO
+
+import Database.TxtSushi.IO
+import Database.TxtSushi.Util.IOUtil
+
+main :: IO ()
+main = do
+    args <- getArgs
+    
+    case args of
+        [fileArg] -> do
+            contents <- getContentsFromFileOrStdin fileArg
+            
+            let table = transpose . parseTable tabDelimitedFormat $ contents
+                tabText = formatTable tabDelimitedFormat table
+            putStr tabText
+        
+        -- we were expecting a single file name arg
+        _ -> printSingleFileUsage
