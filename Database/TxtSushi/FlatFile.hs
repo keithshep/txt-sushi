961d18c67ced810ca400bb9e5a10241727436c60
keithshep <keithshep@gmail.com>
2009-12-25 22:31:13 -0500

    changed code to use parsec as in the Real World Haskell example
    Ignore-this: 638f008a582cb52e5ed3620cef1b3fe
    
    darcs-hash:20091226033113-b3b23-293582a9f1d68168e4ebe956f9162425ee03660f.gz

--- Database/TxtSushi/FlatFile.hs
+++ Database/TxtSushi/FlatFile.hs
@@ -13,27 +13,28 @@ module Database.TxtSushi.FlatFile (
     doubleQuote) where
 
 import Data.List
+import Database.TxtSushi.ParseUtil
+import Text.ParserCombinators.Parsec
 
 {- |
 'Format' allows you to specify different flat-file formats so that you
 can use 'parseTable' for CSV, tab-delimited etc.
 -}
 data Format = Format {
-    quote :: String,
-    fieldDelimiter :: String,
-    rowDelimiter :: String} deriving (Show)
+    quote :: Char,
+    fieldDelimiter :: Char} deriving (Show)
 
 csvFormat :: Format
-csvFormat = Format "\"" "," "\n"
+csvFormat = Format '"' ','
 
 tabDelimitedFormat :: Format
-tabDelimitedFormat = Format "\"" "\t" "\n"
+tabDelimitedFormat = Format '"' '\t'
 
 {- |
 get a quote escape sequence for the given 'Format'
 -}
 doubleQuote :: Format -> String
-doubleQuote format = (quote format) ++ (quote format)
+doubleQuote format = replicate 2 (quote format)
 
 formatTableWithWidths :: String -> [Int] -> [[String]] -> String
 formatTableWithWidths _ _ [] = []
@@ -90,7 +91,7 @@ the given 'Format'
 formatTable :: Format -> [[String]] -> String
 formatTable _ [] = ""
 formatTable format (headRow:tableTail) =
-    (formatRow format headRow) ++ (rowDelimiter format) ++ (formatTable format tableTail)
+    (formatRow format headRow) ++ "\n" ++ (formatTable format tableTail)
 
 {- |
 Format the row into a flat file sub-string using the given 'Format'
@@ -105,19 +106,19 @@ formatRow format (headField:rowTail) =
         if null rowTail then
             escapedField
         else
-            escapedField ++ (fieldDelimiter format) ++ (formatRow format rowTail)
+            escapedField ++ [fieldDelimiter format] ++ (formatRow format rowTail)
 
 {- |
 encode the given text field if it contains any special formatting characters
 -}
 encodeField :: Format -> String -> String
 encodeField format field =
-    if (quote format) `isInfixOf` field then
-        let escapedField = replaceAll field (quote format) (doubleQuote format)
-        in  (quote format) ++ escapedField ++ (quote format)
-    else if (rowDelimiter format) `isInfixOf` field ||
-            (fieldDelimiter format) `isInfixOf` field then
-        (quote format) ++ field ++ (quote format)
+    if quote format `elem` field then
+        let escapedField = replaceAll field [quote format] (doubleQuote format)
+        in  (quote format) : escapedField ++ [quote format]
+    else if '\n' `elem` field ||
+            [fieldDelimiter format] `isInfixOf` field then
+        (quote format) : field ++ [quote format]
     else
         field
 
@@ -134,85 +135,38 @@ replaceAll list@(listHead:listTail) targetSublist replacementList =
     else
         listHead:(replaceAll listTail targetSublist replacementList)
 
+-- flat file parsing (based on Real World Haskell example)
+
+tableParser :: Format -> GenParser Char st [[String]]
+--tableParser format = endBy (line format) eol
+tableParser format = fmap trimLastEmpty (sepBy (line format) eol)
+    where
+        trimLastEmpty [] = []
+        trimLastEmpty [[""]] = []
+        trimLastEmpty (x:xt) = x : trimLastEmpty xt
+
+line :: Format -> GenParser Char st [String]
+line format = sepBy (cell format) (char $ fieldDelimiter format)
+
+cell :: Format -> GenParser Char st String
+cell format = quotedText True (quote format) <|> many (noneOf ",\n\r")
+
+eol :: GenParser Char st String
+eol =   try (string "\n\r")
+    <|> try (string "\r\n")
+    <|> string "\n"
+    <|> string "\r"
+    <?> "end of line"
+
+parseTableOrError :: Format -> String -> Either ParseError [[String]]
+parseTableOrError format input = parse (tableParser format) "(unknown)" input
+
 {- |
 Parse the given text using the given flat file 'Format'. The result
 is a list of list of strings. The strings are fields and the string
 lists are rows
 -}
 parseTable :: Format -> String -> [[String]]
-parseTable _ [] = []
-parseTable format text =
-    let (nextLine, remainingText) = parseLine format text
-    in  nextLine:(parseTable  format remainingText)
-
--- parse a row giving (rowFields, remainingText)
-parseLine :: Format -> String -> ([String], String)
-parseLine _ [] = ([], "")
-parseLine format text =
-    let (nextField, moreFieldsInRow, textRemainingAfterField) = parseField format text
-    in
-        -- if there are more fields, recursively add them to the row
-        if moreFieldsInRow then
-            let (rowTail, remainingText) = parseLine format textRemainingAfterField
-            in  (nextField:rowTail, remainingText)
-        
-        -- if there are no more fields return the current fields as a singleton
-        -- list
-        else
-            ([nextField], textRemainingAfterField)
-
--- parse a field giving (field, moreFieldsInRow, remainingText)
-parseField :: Format -> String -> (String, Bool, String)
-parseField _ [] = ("", False, "")
-parseField format text =
-    -- check if this field is quoted or not
-    if (quote format) `isPrefixOf` text then
-        let tailOfQuote = drop (length (quote format)) text
-        in  parseQuotedField format tailOfQuote
-    else
-        parseUnquotedField format text
-
--- parse a quoted field giving (field, moreFieldsInRow, remainingText)
-parseQuotedField :: Format -> String -> (String, Bool, String)
-parseQuotedField _ [] = ("", False, "")
-parseQuotedField format text@(textHead:textTail) =
-    -- a double quote is an escaped quote, so add a quote to the field
-    if (doubleQuote format) `isPrefixOf` text then
-        let tailOfDoubleQuote = drop (length (doubleQuote format)) text
-            (fieldTail, moreFieldsInRow, remainingText) = parseQuotedField format tailOfDoubleQuote
-        in  ((quote format) ++ fieldTail, moreFieldsInRow, remainingText)
-    
-    -- a single quote is the end of the field, we can use parseUnquotedField to
-    -- chew up any chars between the ending quote and the next delimiter (there
-    -- really shouldn't be any if the text is formatted well, but you never
-    -- know)
-    else if (quote format) `isPrefixOf` text then
-        let tailOfQuote = drop (length (quote format)) text
-            (_, moreFieldsInRow, remainingText) = parseUnquotedField format tailOfQuote
-        in  ("", moreFieldsInRow, remainingText)
-    
-    -- just another character... toss it in the field and keep going
-    else
-        let (fieldTail, moreFieldsInRow, remainingText) = parseQuotedField format textTail
-        in  (textHead:fieldTail, moreFieldsInRow, remainingText)
-
--- parse an unquoted field giving (field, moreFieldsInRow, remainingText)
-parseUnquotedField :: Format -> String -> (String, Bool, String)
-parseUnquotedField _ [] = ("", False, "")
-parseUnquotedField format text@(textHead:textTail) =
-    -- if we hit a field delimiter: return an empty string and let caller know
-    -- there are more fields in this row
-    if (fieldDelimiter format) `isPrefixOf` text then
-        let tailOfDelimiter = drop (length (fieldDelimiter format)) text
-        in  ([], True, tailOfDelimiter)
-    
-    -- if we hit a row delimiter: return an empty string and let caller know there
-    -- are no more fields in this row
-    else if (rowDelimiter format) `isPrefixOf` text then
-        let tailOfDelimiter = drop (length (rowDelimiter format)) text
-        in  ([], False, tailOfDelimiter)
-    
-    -- just another character... toss it in the field and keep going
-    else
-        let (fieldTail, moreFieldsInRow, remainingText) = parseUnquotedField format textTail
-        in  (textHead:fieldTail, moreFieldsInRow, remainingText)
+parseTable format text = case parseTableOrError format text of
+    Left err -> error $ show err
+    Right table -> table
